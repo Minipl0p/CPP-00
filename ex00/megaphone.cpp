a9ffac864efa596cc904e974dafb407d6e7df781
Minipl0p <paul.chazalmartin@protonmail.com>
2026-02-25 16:16:47 +0100

    maj megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,34 +1,20 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: miniplop <miniplop@42angouleme.fr>         +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/27 10:45:30 by miniplop          #+#    #+#             */
-/*   Updated: 2026/02/25 13:06:55 by miniplop         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <iostream>
 #include <cctype>
+#include <string>
+#include <iostream>
 
-int main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-    if (argc == 1)
-    {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return 0;
-    }
-
-    for (int i = 1; i < argc; i++)
+	if (ac <= 1)
 	{
-		for (int j = 0; argv[i][j]; j++)
-        {
-            std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j]))
-            );
-        }
-    }
-    std::cout << std::endl;
-    return 0;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+
+	for (int i = 1; i < ac; i++)
+	{
+		std::string str = static_cast<std::string>(av[i]);
+		for (std::string::iterator it = str.begin(); it != str.end(); it++)
+			std::cout << static_cast<char>(std::toupper(*it));
+	}
+	std::cout << std::endl;
 }
